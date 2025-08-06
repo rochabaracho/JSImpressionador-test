17d79a369153f1fa3a47a99bfd24dec3c495dae9
rochabaracho <lbaracho71@gmail.com>
2025-08-06 12:35:36 -0300

    First commit of the GitHub Project.

---
+++ index.js
@@ -0,0 +1,8 @@
+// Hashtag Programação.
+// JavaScript Impressionador.
+// Luiz Roberto Baracho Rocha.
+// 2025 August, 05
+// 24. Git, Github and ersion control.
+// 24.4. Installing Git and Creating Your Github.
+
+console.log("GitHub Test.");
