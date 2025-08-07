71176d8d40c0f7902c83d377365bc07d057c300f
rochabaracho <lbaracho71@gmail.com>
2025-08-07 10:08:20 -0300

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // JavaScript Impressionador.
 // Luiz Roberto Baracho Rocha.
 // 2025 August, 05
-// 24. Git, Github and ersion control.
+// 24. Git, Github and Version control.
 // 24.4. Installing Git and Creating Your Github.
 
 console.log("GitHub Test.");
